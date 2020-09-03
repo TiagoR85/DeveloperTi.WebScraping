1fda3d1d3c6202f4173e3747649bf06c45caf038
Tiago Rodrigues <tiago.rds85@hotmail.com>
2020-09-03 18:04:42 -0300

    Adicionar arquivos de projeto.

---
+++ DeveloperTi.WebScraping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeveloperTi.WebScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
